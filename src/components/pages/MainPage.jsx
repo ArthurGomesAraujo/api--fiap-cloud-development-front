fa76376d00c7e68be5f9245dce9deac9d43fc75d
ArthurGomesAraujo <arthur.aaraujo@mercadolivre.com>
2022-08-30 22:26:27 -0300

    Changing methods to go inside useEffect

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -13,45 +13,6 @@ const MainPage = () => {
   const [dataset, setDataset] = useState();
   const [refresh, isRefreshing] = useState(false);
 
-  const returnLabels = (data) => {
-    if (data.length > 0) {
-      const labelsArray = [];
-
-      data.forEach((item) => labelsArray.push(cleanData(item.timestamp)));
-
-      return labelsArray;
-    }
-
-    return [];
-  };
-
-  const cleanData = (timestamp) => {
-    const date = new Date(timestamp);
-
-    return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-  };
-
-  const returnDataset = (data) => {
-    if (data.length > 0) {
-      const datasetArray = [];
-      const dataValues = [];
-
-      data.forEach((item) => dataValues.push(item.temperatura));
-
-      datasetArray.push({
-        id: 1,
-        label: 'Sala',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: dataValues,
-      });
-
-      return datasetArray;
-    }
-
-    return [];
-  };
-
   const refreshPage = () => {
     isRefreshing(true);
     if (refresh) {
@@ -60,6 +21,45 @@ const MainPage = () => {
   };
 
   useEffect(() => {
+    const returnLabels = (data) => {
+      if (data.length > 0) {
+        const labelsArray = [];
+
+        data.forEach((item) => labelsArray.push(cleanData(item.timestamp)));
+
+        return labelsArray;
+      }
+
+      return [];
+    };
+
+    const cleanData = (timestamp) => {
+      const date = new Date(timestamp);
+
+      return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    };
+
+    const returnDataset = (data) => {
+      if (data.length > 0) {
+        const datasetArray = [];
+        const dataValues = [];
+
+        data.forEach((item) => dataValues.push(item.temperatura));
+
+        datasetArray.push({
+          id: 1,
+          label: 'Sala',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: dataValues,
+        });
+
+        return datasetArray;
+      }
+
+      return [];
+    };
+
     fetch(`${baseURL}${temperatureList}`)
       .then((response) => response.json())
       .then((data) => {
